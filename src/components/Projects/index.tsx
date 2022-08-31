c3d530d44828ad861b447db93a70ef19508ec5c1
mauro-balades <mauro.balades@tutanota.com>
2022-08-31 19:03:24 +0200

    expanded on projects section

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -1,16 +1,30 @@
 
-import React from "react";
-import { ProjectsWrapper, CloseButton } from "./styles";
-
+import React, { useState } from "react";
+import { ProjectsWrapper, CloseButton, Dots, Dot, Arrow } from "./styles";
+import Snowball from "./pages/snowball";
+import Wonoly from "./pages/wonoly";
 
 function Projects(props: any) {
     const { setClosed } = props;
 
+    const pages = [<Snowball />, <Wonoly />];
+    const [itemNumber, setItemNumber] = useState(0);
+
     return (
         <ProjectsWrapper>
+            {pages[itemNumber]}
             <CloseButton onClick={() => setClosed(true)}>
                 <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" /></svg>
             </CloseButton>
+
+            <Arrow removed={itemNumber == 0} onClick={() => setItemNumber(itemNumber - 1)} className="pointer" direction="left"></Arrow>
+            <Arrow removed={itemNumber == (pages.length-1)} onClick={() => setItemNumber(itemNumber + 1)} className="pointer" direction="right"></Arrow>
+
+            <Dots>
+                {Array.from(Array(pages.length), (_, i) => {
+                    return <Dot isActive={i == itemNumber}></Dot>
+                })}
+            </Dots>
         </ProjectsWrapper>
     )
 }
--- src/components/Projects/styles.ts
+++ src/components/Projects/styles.ts
@@ -4,6 +4,52 @@ import { zoomIn } from 'react-animations'
 
 const zoomInAnimation = keyframes`${zoomIn}`;
 
+export const Arrow = styled.div`
+    ${(props: any) => `
+        position: absolute;
+        top: 50%;
+        ${props.direction}: 5%;
+
+        ${props.removed ? 'transform: translate(-50%, -50%) scale(0) !important;' : ''}
+
+        width: 80px;
+        height: 40px;
+
+        transform: translate(-50%, -50%);
+
+        background: url(/static/svg/arrow-${props.direction}.svg) center no-repeat;
+        background-size: cover;
+
+        transition: .1s;
+        cursor: pointer;
+
+        &:hover {
+            transform: translate(-50%, -50%) scale(1.2) rotate(${props.direction == "right" ? '5' : '-5'}deg);
+        }
+    `}
+`
+
+export const Dot = styled.span`
+    width: 10px;
+    height: 10px;
+    border-radius: 40%;
+
+    background: #fff;
+    margin: 0 5px;
+    ${(props: any) => props.isActive ? `background: #000;` : 'border: 1px solid #000;'}
+`
+
+export const Dots = styled.div`
+    position: absolute;
+    bottom: 5%;
+    left: 50%;
+
+    transform: translate(-50%, -50%);
+
+    display: flex;
+    align-items: center;
+`
+
 export const ProjectsWrapper = styled.div`
     width: 100%;
     height: 100%;
