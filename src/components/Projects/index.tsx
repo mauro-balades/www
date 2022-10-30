d88666fcd3e69f7a5133bd3253e0a5889d619488
mauro-balades <mauro.balades@tutanota.com>
2022-10-30 11:50:33 +0100

    text changes on scroll

--- src/components/Projects/index.tsx
+++ src/components/Projects/index.tsx
@@ -8,6 +8,7 @@ function Projects(props: any) {
 
     const firstSectionRef = useRef(null);
     const scrollSectionRef = useRef(null);
+    const upperTextRef = useRef(null);
 
     const [firstSectionPosition, setFirstSectionPosition] = useState(0);
 
@@ -18,7 +19,8 @@ function Projects(props: any) {
     const onScroll = (e: any) => {
         let section = firstSectionRef.current as any as HTMLElement;
         let scrl_sct = scrollSectionRef.current as any as HTMLElement;
-        console.log(window.innerWidth)
+        let upp_txt = upperTextRef.current as any as HTMLElement;
+
         if (e.deltaY > 0) {
             if ((firstSectionPosition) >= (window.innerWidth - 40)) {
                 setFirstSectionPosition((window.innerWidth));
@@ -33,6 +35,56 @@ function Projects(props: any) {
             }
         }
 
+        let relative_scroll = (window.innerWidth - firstSectionPosition) + 150;
+        console.log(relative_scroll)
+        if (relative_scroll >= 1900) {
+            upp_txt.innerHTML = "// Selected Work";
+        } else if (relative_scroll >= 1850) {
+            upp_txt.innerHTML = "// Selected Wo";
+        } else if (relative_scroll >= 1750) {
+            upp_txt.innerHTML = "// Selected W";
+        } else if (relative_scroll >= 1650) {
+            upp_txt.innerHTML = "// Selected";
+        } else if (relative_scroll >= 1550) {
+            upp_txt.innerHTML = "// Selecte";
+        } else if (relative_scroll >= 1450) {
+            upp_txt.innerHTML = "// Selec";
+        } else if (relative_scroll >= 1450) {
+            upp_txt.innerHTML = "// Sele";
+        } else if (relative_scroll >= 1350) {
+            upp_txt.innerHTML = "// Sel";
+        } else if (relative_scroll >= 1250) {
+            upp_txt.innerHTML = "// Sel";
+        } else if (relative_scroll >= 1200) {
+            upp_txt.innerHTML = "// Se";
+        } else if (relative_scroll >= 1150) {
+            upp_txt.innerHTML = "// S";
+        } else if (relative_scroll >= 1100) {
+            upp_txt.innerHTML = "// ";
+        } else if (relative_scroll >= 950) {
+            upp_txt.innerHTML = "// M";
+        } else if (relative_scroll >= 900) {
+            upp_txt.innerHTML = "// Ma";
+        } else if (relative_scroll >= 850) {
+            upp_txt.innerHTML = "// Mau";
+        } else if (relative_scroll >= 800) {
+            upp_txt.innerHTML = "// Maur";
+        } else if (relative_scroll >= 750) {
+            upp_txt.innerHTML = "// Mauro B";
+        } else if (relative_scroll >= 700) {
+            upp_txt.innerHTML = "// Mauro Ba";
+        } else if (relative_scroll >= 650) {
+            upp_txt.innerHTML = "// Mauro Bal";
+        } else if (relative_scroll >= 600) {
+            upp_txt.innerHTML = "// Mauro Bala";
+        } else if (relative_scroll >= 550) {
+            upp_txt.innerHTML = "// Mauro Baladé";
+        } else if (relative_scroll >= 500) {
+            upp_txt.innerHTML = "// Mauro Baladés";
+        }
+
+        console.log(window.innerWidth - firstSectionPosition)
+
         scrl_sct.style.transform = `translateX(-${firstSectionPosition}px)`;
         section.style.transform = `translateX(-${firstSectionPosition}px)`;
     }
@@ -40,7 +92,7 @@ function Projects(props: any) {
     return (
         <ProjectsWrapper>
             <TopNavigation>
-                <TopNavigationTitle>
+                <TopNavigationTitle ref={upperTextRef}>
                     // Selected Work
                 </TopNavigationTitle>
                 <CloseButton onClick={() => setClosed(true)}>
