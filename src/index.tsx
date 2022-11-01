a7b1220c743a66cb33c6da6f3511ddb5188542d4
mauro-balades <mauro.balades@tutanota.com>
2022-11-01 11:30:17 +0100

    added react route dom for SPA site

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./os/App";
+
+import { HashRouter } from 'react-router-dom';
 import reportWebVitals from "./reportWebVitals";
+
+import App from "./app/App";
 
 const root = ReactDOM.createRoot(
     document.getElementById("root") as HTMLElement
 );
 root.render(
-    // <React.StrictMode>
+    <HashRouter>
         <App />
-    // </React.StrictMode>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
