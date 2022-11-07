10aeaf358b0273cc0e1f7c7272fb4cd66571f7da
mauro-balades <mauro.balades@tutanota.com>
2022-11-07 08:04:09 +0100

    added accent color

--- src/app/snippets/index.ts
+++ src/app/snippets/index.ts
@@ -1,7 +1,9 @@
 import * as nullish_operator from "./nullish_operator.mdx";
 import * as placeholder from "./placeholder_shown.mdx";
+import * as accent_color from "./accent_color.mdx";
 
 export default [
     nullish_operator,
+    accent_color,
     placeholder
 ]
