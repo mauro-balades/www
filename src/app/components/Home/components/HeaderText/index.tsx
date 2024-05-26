da0f3aed0912b89ce6e7da16a3f17fc02aa9960e
mauro 🤙 <mauro.balades@tutanota.com>
2024-05-26 16:06:23 +0000

    Fixed spacing for title

--- src/app/components/Home/components/HeaderText/index.tsx
+++ src/app/components/Home/components/HeaderText/index.tsx
@@ -64,7 +64,7 @@ export default function() {
                     variants={getTransitions(0.1)}>
                     A 
                     <EasterEgg data-cursora>
-                        <span>Software developer</span>
+                        <span> Software developer</span>
                         <img src="/static/gif/cat.gif" />
                     </EasterEgg> that makes stuff. <br />
                 </BigText>
