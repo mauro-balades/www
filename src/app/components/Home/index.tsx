f2e3336603363d588f3429a5d41f15895e7ff096
Definitely not me <96819111+karkutsi@users.noreply.github.com>
2024-06-28 19:11:08 +0000

    started implementing 3d js

--- src/app/components/Home/index.tsx
+++ src/app/components/Home/index.tsx
@@ -1,22 +1,38 @@
 
 import { MotionConfig } from "framer-motion";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useTheme } from "styled-components";
-import { LightThemeNoise } from "../CommonStyles";
-import NavigationBar from "../NavigationBar";
-import HeaderText from "./components/HeaderText";
-import { PageWrapper } from "./style";
+import * as THREE from 'three';
+
 
 export default function() {
     let theme = useTheme();
-    
+    const refContainer = useRef(null);
+
+    useEffect(() => {
+      // === THREE.JS CODE START ===
+      var scene = new THREE.Scene();
+      var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+      var renderer = new THREE.WebGLRenderer();
+      renderer.setSize(window.innerWidth, window.innerHeight);
+      // document.body.appendChild( renderer.domElement );
+      // use ref as a mount point of the Three.js scene instead of the document.body
+      refContainer.current && (refContainer.current as any).appendChild( renderer.domElement );
+      var geometry = new THREE.BoxGeometry(1, 1, 1);
+      var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+      var cube = new THREE.Mesh(geometry, material);
+      scene.add(cube);
+      camera.position.z = 5;
+      var animate = function () {
+        requestAnimationFrame(animate);
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+        renderer.render(scene, camera);
+      };
+      animate();
+    }, []);
     return (
-        <PageWrapper>
-            {theme.theme === "light" && (
-                <LightThemeNoise></LightThemeNoise>
-            )}
-            <NavigationBar></NavigationBar>
-            <HeaderText></HeaderText>
-        </PageWrapper>
-    )
+      <div ref={refContainer}></div>
+  
+    );
 }
